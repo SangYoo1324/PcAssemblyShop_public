1e7ca1d8e9ac2e2b72da1a1d5a0072789187caee
sam <samuel13241@gmail.com>
2023-02-14 23:26:21 -0800

    First Commit

---
+++ src/main/java/com/example/pcAssemblyShop/PcAssemblyShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.pcAssemblyShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcAssemblyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcAssemblyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pcAssemblyShop/PcAssemblyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pcAssemblyShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcAssemblyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
